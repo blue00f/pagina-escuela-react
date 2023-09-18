0cf1ec68f7fd0b198ad585414cd4ccbb06896efc
Jazmin Valenzuela <119680021+jazminValenzuela26@users.noreply.github.com>
2023-09-17 21:32:27 -0300

    Adicionar elementos en 'Inicio'

---
+++ client/src/components/Secciones-Inicio/Secciones.jsx
@@ -0,0 +1,129 @@
+import './Secciones.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+function Secciones(){
+    return(
+    <div _ngcontent-dhg-c19="" class="row">
+        <div _ngcontent-dhg-c19="" class="col-lg-6 mb-3">
+            <a _ngcontent-dhg-c19="" class="card card-horizontal h-100" href="/inicio/tramites-y-servicios/1">
+                <div _ngcontent-dhg-c19="" class="row no-gutters">
+                    <div _ngcontent-dhg-c19="" class="col-md-auto">
+                        <img _ngcontent-dhg-c19="" class="card-img-left" src="https://nba-back-cache.buenosaires.gob.ar/api/v1/imagen/categoria/imagen_categoria_1.svg" alt="Ambiente, espacio público y edilicio"/>
+                    </div>
+                        <div _ngcontent-dhg-c19="" class="col">
+                            <div _ngcontent-dhg-c19="" class="card-body">
+                                <h3 _ngcontent-dhg-c19="" class="card-title">Ambiente, espacio público y edilicio</h3>
+                                <p _ngcontent-dhg-c19="" class="card-text mb-0">Habilitaciones para el uso de la vía pública, espacios urbanos y servicios para el desarrollo sostenible.</p></div></div>
+                </div>
+            </a>
+        </div>
+            <div _ngcontent-dhg-c19="" class="col-lg-6 mb-3">
+                <a _ngcontent-dhg-c19="" class="card card-horizontal h-100" href="/inicio/tramites-y-servicios/19">
+                    <div _ngcontent-dhg-c19="" class="row no-gutters">
+                        <div _ngcontent-dhg-c19="" class="col-md-auto">
+                            <img _ngcontent-dhg-c19="" class="card-img-left" src="https://nba-back-cache.buenosaires.gob.ar/api/v1/imagen/categoria/imagen_categoria_19.svg" alt="Comunidad, arte y cultura"/>
+                        </div>
+                         <div _ngcontent-dhg-c19="" class="col">
+                            <div _ngcontent-dhg-c19="" class="card-body">
+                                <h3 _ngcontent-dhg-c19="" class="card-title">Comunidad, arte y cultura</h3>
+                                <p _ngcontent-dhg-c19="" class="card-text mb-0">Actividades y servicios culturales, asistencia para artistas, gestores y espacios de la cultura.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div _ngcontent-dhg-c19="" class="col-lg-6 mb-3">
+                <a _ngcontent-dhg-c19="" class="card card-horizontal h-100" href="/inicio/tramites-y-servicios/40">
+                    <div _ngcontent-dhg-c19="" class="row no-gutters">
+                        <div _ngcontent-dhg-c19="" class="col-md-auto">
+                            <img _ngcontent-dhg-c19="" class="card-img-left" src="https://nba-back-cache.buenosaires.gob.ar/api/v1/imagen/categoria/imagen_categoria_40.svg" alt="Denuncias, emergencia y seguridad pública"/>
+                        </div>
+                        <div _ngcontent-dhg-c19="" class="col">
+                            <div _ngcontent-dhg-c19="" class="card-body">
+                                <h3 _ngcontent-dhg-c19="" class="card-title">Denuncias, emergencia y seguridad pública</h3>
+                                <p _ngcontent-dhg-c19="" class="card-text mb-0">Reclamos, denuncias y solicitudes ante incumplimientos, situaciones irregulares y emergencias.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div _ngcontent-dhg-c19="" class="col-lg-6 mb-3">
+                <a _ngcontent-dhg-c19="" class="card card-horizontal h-100" href="/inicio/tramites-y-servicios/45">
+                    <div _ngcontent-dhg-c19="" class="row no-gutters">
+                        <div _ngcontent-dhg-c19="" class="col-md-auto">
+                            <img _ngcontent-dhg-c19="" class="card-img-left" src="https://nba-back-cache.buenosaires.gob.ar/api/v1/imagen/categoria/imagen_categoria_45.svg" alt="Educación y formación"/>
+                        </div>
+                        <div _ngcontent-dhg-c19="" class="col">
+                            <div _ngcontent-dhg-c19="" class="card-body">
+                                <h3 _ngcontent-dhg-c19="" class="card-title">Educación y formación</h3>
+                                <p _ngcontent-dhg-c19="" class="card-text mb-0">Prácticas y servicios de educación formal, no formal y formación laboral para toda la ciudadanía.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div _ngcontent-dhg-c19="" class="col-lg-6 mb-3">
+                <a _ngcontent-dhg-c19="" class="card card-horizontal h-100" href="/inicio/tramites-y-servicios/58">
+                    <div _ngcontent-dhg-c19="" class="row no-gutters">
+                        <div _ngcontent-dhg-c19="" class="col-md-auto">
+                            <img _ngcontent-dhg-c19="" class="card-img-left" src="https://nba-back-cache.buenosaires.gob.ar/api/v1/imagen/categoria/imagen_categoria_58.svg" alt="Identidad, familias y cuidados"/>
+                        </div>
+                        <div _ngcontent-dhg-c19="" class="col">
+                            <div _ngcontent-dhg-c19="" class="card-body">
+                                <h3 _ngcontent-dhg-c19="" class="card-title">Identidad, familias y cuidados</h3>
+                                <p _ngcontent-dhg-c19="" class="card-text mb-0">Documentación ciudadana, asistencia social, acompañamiento económico y programas de desarrollo para familias.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div _ngcontent-dhg-c19="" class="col-lg-6 mb-3">
+                <a _ngcontent-dhg-c19="" class="card card-horizontal h-100" href="/inicio/tramites-y-servicios/68">
+                    <div _ngcontent-dhg-c19="" class="row no-gutters">
+                        <div _ngcontent-dhg-c19="" class="col-md-auto">
+                            <img _ngcontent-dhg-c19="" class="card-img-left" src="https://nba-back-cache.buenosaires.gob.ar/api/v1/imagen/categoria/imagen_categoria_68.svg" alt="Salud y deportes"/>
+                        </div>
+                        <div _ngcontent-dhg-c19="" class="col">
+                            <div _ngcontent-dhg-c19="" class="card-body">
+                                <h3 _ngcontent-dhg-c19="" class="card-title">Salud y deportes</h3>
+                                <p _ngcontent-dhg-c19="" class="card-text mb-0">Actividades deportivas, servicios de prevención y cuidados para la salud de la población.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div _ngcontent-dhg-c19="" class="col-lg-6 mb-3">
+                <a _ngcontent-dhg-c19="" class="card card-horizontal h-100" href="/inicio/tramites-y-servicios/83">
+                    <div _ngcontent-dhg-c19="" class="row no-gutters">
+                        <div _ngcontent-dhg-c19="" class="col-md-auto">
+                            <img _ngcontent-dhg-c19="" class="card-img-left" src="https://nba-back-cache.buenosaires.gob.ar/api/v1/imagen/categoria/imagen_categoria_83.svg" alt="Trabajo, industrias y empresas"/>
+                        </div>
+                        <div _ngcontent-dhg-c19="" class="col">
+                            <div _ngcontent-dhg-c19="" class="card-body">
+                                <h3 _ngcontent-dhg-c19="" class="card-title">Trabajo, industrias y empresas</h3>
+                                <p _ngcontent-dhg-c19="" class="card-text mb-0">Líneas de financiamiento, permisos, registros y solicitudes para el desarrollo de actividades económicas y laborales.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div _ngcontent-dhg-c19="" class="col-lg-6 mb-3">
+                <a _ngcontent-dhg-c19="" class="card card-horizontal h-100" href="/inicio/tramites-y-servicios/89">
+                    <div _ngcontent-dhg-c19="" class="row no-gutters">
+                        <div _ngcontent-dhg-c19="" class="col-md-auto">
+                            <img _ngcontent-dhg-c19="" class="card-img-left" src="https://nba-back-cache.buenosaires.gob.ar/api/v1/imagen/categoria/imagen_categoria_89.svg" alt="Tránsito y movilidad"/>
+                        </div>
+                        <div _ngcontent-dhg-c19="" class="col">
+                            <div _ngcontent-dhg-c19="" class="card-body">
+                                <h3 _ngcontent-dhg-c19="" class="card-title">Tránsito y movilidad</h3>
+                                <p _ngcontent-dhg-c19="" class="card-text mb-0">Servicios de transporte público, información para circular en vía pública, trámites de movilidad y seguridad vial.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+    </div>
+    )
+}
+
+export default Secciones
