365f561562a74ffbf844454d4a6ce420f79a895b
Bruno <brunoleonelroca@gmail.com>
2023-10-07 16:55:54 -0300

    Modificar publicaciones

--- client/src/components/CardPublicacion/CardPublicacion.jsx
+++ client/src/components/CardPublicacion/CardPublicacion.jsx
@@ -5,7 +5,7 @@ function CardPublicacion({ title, content, dateUploaded }) {
   return (
     <a href="/">
       <div className="contenedor-img">
-        <img src="./fotoEscuela.jpg" alt="Entrada de la escuela" />
+        <img src="./foto-escuela-2.jpg" alt="Entrada de la escuela" />
         <div className="descripcion-superior-div">
           <h3 className="texto-bold">{title}</h3>
           <p className="texto-regular">{content}</p>
